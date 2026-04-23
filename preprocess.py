c15b079717b1f49df127f3c97070efbd0de42845
kvohra01 <karan.vohra@tufts.edu>
2026-04-23 13:25:26 -0500

    preprocess and save new dataset

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,19 @@
+from datasets import load_dataset
+from PIL import Image
+import numpy as np
+import os
 
+def preprocess_example(example):
+    img = example["image"].convert("RGB")
+    img = img.resize((256, 256), Image.BILINEAR)
+    example["pixel_values"] = np.array(img)
+    return example
+
+if __name__ == "__main__":
+    dataset = load_dataset("lucabaggi/animal-wildlife")
+
+    processed_dataset = dataset.map(
+        preprocess_example,
+        num_proc=os.cpu_count() - 1
+    )
+    processed_dataset.save_to_disk("processed_animals_256")
